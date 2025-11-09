6d95bb3d2fa79f4d3edc8bf9e37462412a666d66
GabrielDichavado <gabrieloliveirasouto158@gmail.com>
2025-11-09 18:49:48 -0300

    v1.9.2
    Js basico aplicado a página info.html pensado para futuras funcionalidades e aplicações pensadas para a página.

---
+++ eco-ponto/public/js/info.js
@@ -0,0 +1,5 @@
+// /js/info.js
+// Arquivo reservado para interações da página info.html
+// Por enquanto o conteúdo estático do HTML é suficiente.
+// Se quiser animações ou expand/collapse nos cards eu adiciono.
+console.log("info.js carregado");
